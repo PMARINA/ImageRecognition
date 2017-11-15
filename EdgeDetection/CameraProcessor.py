86104ee72f26ad5e33357732ed55f0accb17223a
PMARINA <bluerat33@gmail.com>
2017-11-15 18:37:13 -0500

    .

--- EdgeDetection/CameraProcessor.py
+++ EdgeDetection/CameraProcessor.py
@@ -4,25 +4,33 @@ Version: Python 2.7.13
 
 Instructions:
 1) Read all the instructions before you do anything
-2) Change the preferences below to match your personal preferences and the machine on which you will be running the script
+2) Change the preferences below to match your personal preferences and the
+    machine on which you will be running the script
 3) Run the program
-4) Use 'q' and 'e' keys to play with the gaussian blur settings: 'q' makes it go less (starting at the minimum, so it won't go any further) and 'e' makes it blur more.
-5) Use 'w', 'a', 's', 'd' keys to play with the boundaries for the Canny algorithm. Use 'a' and 'd' to modify the lower boundary, and 'w' and 's' to modify the upper boundary.
-6) This is a continuation of 5, but I ran out of space: 'a' and 's' are for making the value smaller, 'd' and 'w' are for making it bigger.
+4) Use 'q' and 'e' keys to play with the gaussian blur settings: 'q' makes it go
+    less (starting at the minimum, so it won't go any further) and 'e' makes it
+    blur more.
+5) Use 'w', 'a', 's', 'd' keys to play with the boundaries for the Canny
+    algorithm. Use 'a' and 'd' to modify the lower boundary, and 'w' and 's' to
+    modify the upper boundary. 'a' and 's' are for making the value smaller,
+    'd' and 'w' are for making it bigger.
 '''
 import cv2  # Yes, you need OpenCV Installed
 
 # Constants to be initialized based on the user's machine and preferences
-WIDTH = 1280  # Width of the display
-HEIGHT = 720  # Height of the display
-# Making a tuple because I'm lazy (hint: not a setting to be changed)
+WIDTH = 1280  # Width of the display window
+HEIGHT = 720  # Height of the display window
+# Please don't modify the following line, as it contains a constant in the form
+# of a tuple, which I'm using to make my code less complex
 DIMENSIONS = (WIDTH, HEIGHT)
-# If your device has multiple camera interfaces, use this to select which one you would like to select
+# If your device has multiple camera interfaces, use this to select which one
+# you would like to select
 INPUTdEVICEnUMBER = 0
 
 '''
 This method exists to return the inverted color version of the image submitted.
-Note that this method only works SUCCESSFULLY on black and white images. Grayscale not supported.
+Note that this method only works SUCCESSFULLY on black and white images.
+Grayscale not supported.
 '''
 
 
@@ -33,13 +41,15 @@ def invertBlackAndWhite(img):
 
 
 '''
-This method returns the contours of a given image, after taking a gaussian blur, to remove image noise.
-The user, through his/her key inputs can select the gaussian blur settings and the contour detection method parameters
+This method returns the contours of a given image, after taking a gaussian blur,
+to remove image noise. The user, through his/her key inputs can select the
+gaussian blur settings and the contour detection method parameters
 '''
 
 
 def contoursOnly(image, boundryLower, boundryUpper, num):
-    # Use a gaussian blur to get rid of image noise, which will become annoying in the next step
+    # Use a gaussian blur to get rid of image noise, which will become annoying
+    # in the next step
     noiselessImage = cv2.GaussianBlur(image, (num, num), 0)
     # Get the contours of the img using Canny's algorithm
     contours = cv2.Canny(noiselessImage, boundryLower, boundryUpper)
@@ -61,8 +71,12 @@ Main Logic of Program
 cap = cv2.VideoCapture(INPUTdEVICEnUMBER)  # Get the camera
 boundryUpper = 35  # Set the initial boundaries, Note that these are numbers
 boundryLower = 10  # that I've found work well for my system.
-num = 1  # Number for Gaussian Blur (must be >0). At num=1, there is no smoothing. At greater values, you get more smoothing, though this isn't necessarily great, as it can result in the edges becoming obscured or not detected
-# Debugging statement, as some computers may hang on the initialization part, or outright fail when attempting to use the camera, if none is attached
+# Number for Gaussian Blur (must be >0). At num=1, there is no smoothing.
+num = 1
+# At greater values, you get more smoothing, though this isn't necessarily great,
+# as it can result in the edges becoming obscured or not detected
+# Debugging statement, as some computers may hang on the initialization part,
+# or outright fail when attempting to use the camera, if none is attached
 print("Camera has been successfully initialized.")
 while True:
     # ret = whether or not a frame was returned, img is the frame taken at the instant the method was called
